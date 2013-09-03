ea17b5b22f0a59bbb92f1818e8d4fd0bc4387c4a
Robin Roestenburg <robin@robins-macbook-pro.fritz.box>
2013-09-03 23:02:49 +0200

    Added prompt

--- config/local.zsh
+++ config/local.zsh
@@ -1,9 +1,28 @@
-export PATH="~/bin:/usr/local/bin:/usr/local/sbin:$PATH"
+# Color codes in zsh (used mainly in prompt coloring)
+export reset=$'%{\e[0;00m%}'
+export gray=$'%{\e[0;90m%}'
+export red=$'%{\e[0;31m%}'
+export green=$'%{\e[0;32m%}'
+export yellow=$'%{\e[0;33m%}'
+export blue=$'%{\e[0;34m%}'
+export magenta=$'%{\e[0;35m%}'
+export cyan=$'%{\e[0;36m%}'
+export white=$'%{\e[0;37m%}'
 
+
+HISTFILE=~/.histfile
+HISTSIZE=10000
+SAVEHIST=10000
+
+# Set prompt
+setopt prompt_subst
+PROMPT='$(~/.dotfiles/script/prompt $?)'
+
+export SHELL=$(which zsh)
+
+export PATH="~/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 export EDITOR="vim"
 
-## ALIASES
-alias vi="/usr/local/bin/vim"
 
 ## CHRUBY
 
