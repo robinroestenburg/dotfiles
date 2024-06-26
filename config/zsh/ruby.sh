aefe76a6cfc3ef5ca0fd8172308147c17b9dcf36
Robin Roestenburg <robin.roestenburg@itrp.com>
2024-06-26 12:06:52 +0200

    Various changes

--- config/zsh/ruby.sh
+++ config/zsh/ruby.sh
@@ -1,10 +1,11 @@
 # Ruby aliases
-alias rdm='rake db:migrate db:test:prepare'
+alias be='bundle exec'
+alias rdb='be rake db:test:prepare'
+
 alias rr='mkdir -p tmp && touch tmp/restart.txt'
 alias c='bundle exec cucumber -r features'
 alias wip='c --profile wip'
 # alias specdoc='time rspec -fd'
 # alias s='rspec --require ~/.dotfiles/script/rspec_focus --order default --color --tty'
-alias be='bundle exec'
 alias irb='pry'
 # alias guard='bundle exec guard'
